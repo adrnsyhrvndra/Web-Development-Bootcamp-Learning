e269d61f75f0a0c8296e1a1c9431f1ce4d261b6e
Adriansyah Ravindra <adrimedia.indonesia@gmail.com>
2023-02-09 15:22:00 +0700

    Add Javascript DOM Lesson
    Ini merupakan commit saya belajar bagian javascript DOM dasar.

---
+++ JAVASCRIPT DOM/script.js
@@ -0,0 +1,20 @@
+
+// alert('Hello World');
+
+// document.querySelector("h1").innerHTML = "fools";
+
+// document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Angela"
+
+document.getElementById('list').style.color = "orange";
+
+document.getElementById('linkid').style.color = "orange";
+
+document.getElementById('linkid').style.fontSize = '40px';
+
+document.querySelector("button").style.backgroundColor = "red";
+
+document.querySelector('h1').classList.add('huge');
+
+document.querySelector("h1").textContent = "fools";
+
+document.querySelector("a").setAttribute("href","https://www");
